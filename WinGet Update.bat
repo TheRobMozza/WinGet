f8b4b938cf5ce947e5025ba940ee0aa6bba47228
TheRobMozza <8695295+TheRobMozza@users.noreply.github.com>
2023-01-16 23:03:34 +0000

    Update WinGet Update.bat

--- WinGet Update.bat
+++ WinGet Update.bat
@@ -133,6 +133,8 @@ winget list %app% | findstr /c:"No installed package found" > nul && (
 ) || (
     winget upgrade %app% --force --silent
 )
+CD\"Program Files"\SUPERAntiSpyware\
+SUPERAntiSpyware.exe
 GOTO exit
 
 :sasNo
