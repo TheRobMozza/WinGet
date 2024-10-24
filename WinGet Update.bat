0e9b5ab56578fd86ed74c3fc6c3d9eae4441506b
TheRobMozza <8695295+TheRobMozza@users.noreply.github.com>
2024-10-24 02:04:39 +0100

    Update WinGet Update.bat

--- WinGet Update.bat
+++ WinGet Update.bat
@@ -1,19 +1,33 @@
 @ECHO OFF
 ::
-:: WinGet 3rd Party Update Automated Batch File
+:: WinGet 3rd Party Automated Update Batch File
 :: By TheRobMozza - https://github.com/TheRobMozza/
 ::
+
+:: *****************************************************
+SET CLEANUP=0
+::^^ 0 = Disabled - 1 = Enabled ^^::
+SET CLEANUPLOCATION="C:\Users\YOURUSERNAME\Scripts\CleanUp.bat"
+::^^ YOUR-CLEANUP-BATCH-FILE-LOCATION-HERE ^^::
+:: *****************************************************
+
+:: Sets the ESC character for coloured text 
+for /F %%a in ('echo prompt $E ^| cmd') do set "ESC=%%a"
+:: Colours available = https://i.sstatic.net/gpDPE.png
+
 echo.
-echo Your OS is : && ver
+echo Your OS is : %ESC%[91m && ver && echo %ESC%[0m
 echo.
 echo.
 echo                                          ------------------------------------------
 echo.
 echo                                           Checking for Administrator privileges...
 echo.
+echo						  TO CANCEL OPERATION JUST HIT "NO"
+echo.
 echo                                          ------------------------------------------
 echo.
-TIMEOUT 5
+TIMEOUT 3
 NET SESSION 2>nul
 IF %ERRORLEVEL% NEQ 0 GOTO elevate
 GOTO adminTasks
@@ -27,7 +41,7 @@ GOTO exit
 echo.
 echo                                          ------------------------------------------
 echo.
-echo                                           WinGet is now running as Administrator..
+echo                                           WinGet is now running as %ESC%[1mAdministrator..%ESC%[0m
 echo.
 echo                                          ------------------------------------------
 echo.
@@ -41,7 +55,7 @@ echo                                          ----------------------------------
 echo.
 echo                                           Check for 3rd party program updates now?
 echo.
-echo                                           - (Y)es or (N)o - Default is YES (5 sec)
+echo                                           - (Y)es or (N)o - %ESC%[43mDefault is YES (5 sec)%ESC%[0m
 echo.
 echo                                          ------------------------------------------
 echo.
@@ -52,16 +66,16 @@ IF %ERRORLEVEL% == 2 GOTO updatesNo
 GOTO choice1
 
 :choice2
-echo Checking for Discord running in memory, if found stopping Discord... 
+echo Checking for Discord running in memory, if found stopping Discord as it hinders its own update operation... 
 echo.
-tasklist | find /i "discord.exe" && taskkill /IM discord.exe /F > NUL || echo. && echo Process "discord.exe" was not found running..
+tasklist | find /i "discord.exe" && taskkill /IM discord.exe /F >nul || echo. && echo Process "discord.exe" was not found running..
 echo.
 echo.
 echo                                          ----------------------------------------------
 echo.
 echo                                           Would you like to include 'unknown' updates?
 echo.
-echo                                           - (Y)es or (N)o - Default is NO (5 seconds)
+echo                                           - (Y)es or (N)o - %ESC%[43mDefault is NO (5 seconds)%ESC%[0m
 echo.
 echo                                          ----------------------------------------------
 echo.
@@ -74,12 +88,12 @@ GOTO choice2
 
 :updatesAllYes
 PowerShell.exe -ExecutionPolicy Bypass -Command "winget upgrade --all --include-unknown"
-TIMEOUT 1 > NUL
+TIMEOUT 1 >nul
 GOTO winGetCheck
 
 :updatesYes
 PowerShell.exe -ExecutionPolicy Bypass -Command "winget upgrade --all"
-TIMEOUT 1 > NUL
+TIMEOUT 1 >nul
 GOTO winGetCheck
 
 :winGetCheck
@@ -89,9 +103,9 @@ echo                                     ---------------------------------------
 echo.
 echo                                      Did you get the following error message?
 echo.
-echo                                      "Failed in attempting to update the source: winget"
+echo                                      %ESC%[1m"Failed in attempting to update the source: winget"%ESC%[0m
 echo.
-echo                                      - (Y)es I did or (N)o I didn't - Default No (5 sec)
+echo                                      - (Y)es I did or (N)o I didn't - %ESC%[43mDefault No (5 sec)%ESC%[0m
 echo.
 echo                                     -----------------------------------------------------
 echo.
@@ -105,9 +119,9 @@ GOTO choice3
 echo.
 echo                                        -----------------------------------------
 echo.
-echo                                         Forcing WinGet to reset to default
+echo                                         Forcing WinGet to reset to %ESC%[1mDEFAULTS%ESC%[0m
 echo.
-echo                                         You will have to re-run this batch file
+echo                                         You may have to re-run this batch file
 echo.
 echo                                        -----------------------------------------
 echo.
@@ -130,9 +144,9 @@ echo                                             (Known problems in the past...)
 echo.
 echo                                        ------------------------------------------
 echo.
-tasklist | find /i "superantispyware.exe" && taskkill /IM superantispyware.exe /F > NUL || echo. && echo Process "superantispyware.exe" was not found running..
+tasklist | find /i "superantispyware.exe" && taskkill /IM superantispyware.exe /F >nul || echo. && echo Process "superantispyware.exe" was not found running..
 set app=superantispyware
-winget list %app% | findstr /c:"No installed package found" > nul && (
+winget list %app% | findstr /c:"No installed package found" >nul && (
     winget install %app% --force --silent
 ) || (
     winget upgrade %app% --force --silent
@@ -144,12 +158,18 @@ GOTO exit
 echo.
 echo                                           ==================================
 echo.
-echo                                            All Updates have been COMPLETED!
+echo                                            %ESC%[93mAll Updates have been COMPLETED!%ESC%[0m
 echo.
 echo                                                 Quitting in 3 seconds..
 echo.
 echo                                           ==================================
-TIMEOUT 3
+TIMEOUT 3 >nul
+GOTO cleanup
+
+:cleanup 
+echo. && echo. && echo *************************************************** && echo. && echo Running Automated Desktop Clean Up if applicable. . && echo. && echo ****************************************************
+TIMEOUT 3 >nul
+IF %CLEANUP%==1 (echo. && echo Clean UP is ENABLED && TIMEOUT 1 >nul && echo. && call %CLEANUPLOCATION%) ELSE (echo. && echo Clean Up is DISABLED && TIMEOUT 3 >nul)
 GOTO exit
 
 :updatesNo
@@ -161,7 +181,7 @@ echo.
 echo                                               Quitting in 3 seconds..
 echo.
 echo                                              -------------------------
-TIMEOUT 3
+TIMEOUT 3 >nul
 GOTO exit
 
 :exit
